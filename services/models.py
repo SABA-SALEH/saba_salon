edfacdefec7adb258c7ebbc4da517cd1757d8b50
Saba Saleh <m.sweet90@gmail.com>
2024-06-19 23:24:04 +0000

    added services app , models and fixtures

---
+++ services/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+class Service(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='services')
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    duration = models.DurationField(null=True, blank=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image = models.ImageField(upload_to='services/', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
