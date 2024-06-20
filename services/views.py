b711fe67e0b3a97654e78c999673a56e91333980
Saba Saleh <m.sweet90@gmail.com>
2024-06-20 23:21:50 +0000

    ADDED SEARCH FUNCTIONALITY AND CATEGORY FILTERING

--- services/views.py
+++ services/views.py
@@ -1,16 +1,38 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Service
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
+from .models import Service, Category
 
 def all_services(request):
     """ A view to show all services, including sorting and search queries """
-    
+
     services = Service.objects.all()
+    query = None
+    categories = Category.objects.all()
+
+    if request.GET:
+        if 'category' in request.GET:
+            category_names = request.GET.getlist('category')
+            services = services.filter(category__name__in=category_names)
+            categories = Category.objects.filter(name__in=category_names)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('services:all_services'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            services = services.filter(queries)
 
     context = {
         'services': services,
+        'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'services/services.html', context)
+
 
 def service_detail(request, service_id):
     """ A view to show individual service details """
