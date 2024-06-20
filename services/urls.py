94489f9d03bd3fe0b5f4271db522076bffdca94f
Saba Saleh <m.sweet90@gmail.com>
2024-06-20 20:23:06 +0000

    added service detail functionality

--- services/urls.py
+++ services/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'services'
 urlpatterns = [
-    path('', views.all_services, name='all_services')
+    path('', views.all_services, name='all_services'),
+    path('<int:service_id>/', views.service_detail, name='service_detail'),
 ]
