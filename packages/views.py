7917ab3d1780193d976de7f54e84a33b8deb236c
Saba Saleh <m.sweet90@gmail.com>
2024-07-11 21:21:35 +0000

    modified pages style

--- packages/views.py
+++ packages/views.py
@@ -3,11 +3,13 @@ from .models import Package
 
 # Create your views here.
 
-
 def package_list(request):
     packages = Package.objects.all()
-    return render(request, 'packages/package_list.html', {'packages': packages})
+    best_deal_package = max(packages, key=lambda package: package.saving_price, default=None)
 
-
-
+    context = {
+        'packages': packages,
+        'best_deal_package': best_deal_package,
+    }
+    return render(request, 'packages/package_list.html', context)
    
