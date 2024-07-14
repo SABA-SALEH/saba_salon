4ba486b6e7c8e215989fb7bad0dddd4f52ccfc61
Saba Saleh <m.sweet90@gmail.com>
2024-07-14 16:04:18 +0000

    added webhook handler and webhook view

--- checkout/urls.py
+++ checkout/urls.py
@@ -6,4 +6,5 @@ app_name = 'checkout'
 urlpatterns = [
     path('', views.checkout, name='checkout'),  
     path('success/<str:order_number>/<str:email>/', views.checkout_success, name='checkout_success'),
+    path('wh/', webhook, name='webhook'),
 ]
