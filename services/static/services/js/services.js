68e81c3efa4d76accf67f7faf28bad86bc55be6a
Saba Saleh <m.sweet90@gmail.com>
2024-08-17 19:33:04 +0000

    edit service detail

--- services/static/services/js/services.js
+++ services/static/services/js/services.js
@@ -115,29 +115,3 @@ document.getElementById('toggleAdditionalService').addEventListener('click', fun
         event.target.textContent = 'Add Another Service';
     }
 });
-
-
-// Handle SweetAlert2 delete confirmation
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('.delete-link').forEach(link => {
-        link.addEventListener('click', function (event) {
-            event.preventDefault();
-            const url = this.getAttribute('data-url');
-            Swal.fire({
-                title: 'Are you sure?',
-                text: 'You won’t be able to revert this!',
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#CA8787',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Yes, delete it!',
-                cancelButtonText: 'Cancel'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    window.location.href = url;  // Redirect to the URL for deletion
-                }
-            });
-        });
-    });
-});
-
